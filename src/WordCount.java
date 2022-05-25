3ddae3e4f42539192ea37dcdc961b866bf11b647
Jayden <jayden.chae1324@gmail.com>
2022-05-24 18:05:33 -0700

    as;elkjfd

--- src/WordCount.java
+++ src/WordCount.java
@@ -3,9 +3,23 @@ import java.util.*;
 import java.io.File;
 public class WordCount {
     public static void main(String[] args) throws FileNotFoundException {
+        Map<String, Integer> wordCounts = new HashMap<>();
         File alice = new File("alice.txt"); // gets file
         Scanner fileLineScanner = new Scanner(alice); // loads text into scanner
-        System.out.println(fileLineScanner.nextLine()); // prints out first line in alice.txt, feel free to change or delete anything!
+
+        while(fileLineScanner.hasNextLine() == true) {
+            String line = fileLineScanner.nextLine();
+            Scanner wordScanner = new Scanner(line);
+            String word = wordScanner.next();
+            if(wordCounts.containsKey(word) == false) {
+                wordCounts.put(word, 1);
+            }
+             else if (wordCounts.containsKey(word) == true){
+                wordCounts.put(word, wordCounts.get(word) + 1);
+            }
+
+        }
+        System.out.println(wordCounts.values()); // prints out first line in alice.txt, feel free to change or delete anything!
         // Probably want to scan by lines first and then scanner each string with a different scanner...
         // A hashmap might also be very useful in this project...
     }
