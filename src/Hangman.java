8b46007a067bbfd942c019530dbb099ec3bd1517
Jayden <jayden.chae1324@gmail.com>
2022-05-31 18:02:35 -0700

    as;elkjfd

--- src/Hangman.java
+++ src/Hangman.java
@@ -9,14 +9,29 @@ import java.io.*;
 
 public class Hangman {
     public static void main(String args[]) throws FileNotFoundException{
+        Scanner scanner = new Scanner(System.in);
         String[] possibleWords = {"difference", "array"}; // feel free to add any new words!
         Random rand = new Random();
         String randomWord = possibleWords[rand.nextInt(possibleWords.length)]; // gets random word from possibleWords
 
+        int guessesLeft = 5;
+        System.out.println("Guess the word");
         // Feel free to change any code or used this existing code!
-        while(some condition here){
+        while(scanner.hasNext() && guessesLeft > 1){
+
+            String guess = scanner.next();
             // get user input, then probably check it in some way...
             // Also need to give user feed back on if the guess was right or not and the game status
+
+            if(guess.equalsIgnoreCase(randomWord) == true) {
+                System.out.println("correct");
+            } else {
+                guessesLeft--;
+                System.out.println("incorrect, " + guessesLeft + " guesses remaining");
+
+
+
+            }
         }
     }
 
